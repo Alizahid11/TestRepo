f2477fb65a4cef816ead860a742c83c2682c562d
Alizahid11 <zahid_sakhizada@yahoo.com>
2019-04-15 13:34:14 +0100

    Init Commit

---
+++ src/main/java/Convert.java
@@ -0,0 +1,35 @@
+
+public class Convert {
+
+	public String sick(int n){
+
+		int a;
+		String x = " ";
+
+				
+				while (n>0){
+				a = n % 2;
+				
+				
+						x = a + "" + x;
+						n = n/2;
+					}
+					return x;
+		}
+			public float hmmm(String x){
+
+		int n; 
+		float a;
+		n=0;
+		a=0;
+		
+
+			for (int i =x.length(); i>0; i--,n++){
+						a+= Integer.parseInt(x.substring(i-1,i)) * Math.pow(2,n);
+
+					}
+
+						return a;
+	}
+	
+}
